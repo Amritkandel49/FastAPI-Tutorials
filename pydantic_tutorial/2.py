e8210515253bb2bdfc4d3bb6c6725c6c4f65e0c0
Amritkandel49 <amritkandel062@gmail.com>
2025-08-06 18:38:25 +0545

    Learnt about Annotated function

--- pydantic_tutorial/2.py
+++ pydantic_tutorial/2.py
@@ -1,25 +1,33 @@
 #  building more complex model then in 1.py
-from pydantic import BaseModel, EmailStr
-from typing import List, Optional
+from pydantic import BaseModel, EmailStr, AnyUrl, Field
+from typing import List, Optional, Annotated
 
 
 # Pydantic also provides various built-in complex data types line EmailStr to validate email addresses 
 
 class Patient(BaseModel):
-    name: str
+    # name: str = Field(max_length=50)
+    # we can use Annotated to add metadata to field as below
+    name: Annotated[str, Field(max_length=50, description="Name of the patient, max length 50 characters", example="John Doe", title="Patient Name")]
+
     email: EmailStr
+    linkedin_url: Optional[AnyUrl] = None # AnyUrl validates the format of the given URL
     age: int
-    weight: float
-    allergies: Optional[List[str]] = None 
-    married: bool = False
+    weight: Annotated[float, Field(gt = 0, description="Weight of the patient in kg, must be greater than 0", strict= True)]
+    allergies: Annotated[Optional[List[str]], Field(max_length=5, default=None)] # List of strings with a maximum length of 5
+    married: Annotated[bool, Field(default = False, description="Marital status of the patient")]
     contact_details: dict[str, str]
 
 patient_info = {
     "name": "John Doe",
     "email": "abs@gmail.com",
+    "linkedin_url": "https://www.linkedin.com/in/johndoe",
+    # "linkedin_url": "not a valid url",  # Uncommenting this will raise
     "age": 30,
     "weight": 70.5,
-    # "allergies": ["dust", "pollens"],
+    # "weight": -1, # This will raise a validation error because weight must be greater than 0
+
+    "allergies": ["dust", "pollens"],
     # "married": True,
     "contact_details": {
         "phone": "123-456-7890"
@@ -32,6 +40,7 @@ def get_patient_info(patient: Patient):
     print(f'''
         "name": {patient.name},
         "email": {patient.email},
+        "linkedin_url": {patient.linkedin_url},
         "age": {patient.age},
         "weight": {patient.weight},
         "allergies": {patient.allergies},
